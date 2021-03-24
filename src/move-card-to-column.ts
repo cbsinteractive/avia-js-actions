4692d1008938e3cd9952efcaa951eeb8aca89bea
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-03-24 16:39:59 -0700

    no message

--- src/move-card-to-column.ts
+++ src/move-card-to-column.ts
@@ -4,10 +4,8 @@ import { context, getCard, getColumnIdByName, moveExistingCard } from './octokit
 const projectUrl = 'https://github.com/cbsinteractive/github-actions-test/projects/2';
 
 export default async function moveCardToColumn() {
-  console.log("moveCardToColumn", context.payload);
   const branch = context.payload.pull_request.head.ref;
   const issueNumber = branch.split('/').pop();
-  console.log('ISSUE lookup: ', issueNumber, branch);
   const card = await getCard(issueNumber, 'In Progress', projectUrl);
   const toColumn = await getColumnIdByName('Ready for Review', projectUrl, null);
 
