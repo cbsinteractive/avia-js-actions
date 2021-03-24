29825eca6546353e914916eb78d5cc8ae4bbacb9
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-03-24 12:42:47 -0700

    no message

--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parallel = void 0;
-function parallel(...args) {
-    return Promise.all(args);
-}
-exports.parallel = parallel;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import * as core from '@actions/core';
+import updateCard from './update-card';
 
 async function run() {
   try {
-
     const action = core.getInput('action') || process.env.ACTION || 'update-card';
-    const run = (await import(`${action}.js`)).default;
-    await run();
+
+    switch (action) {
+      case 'update-card':
+        await updateCard();
+        break;
+    }
   }
   catch (error) {
     core.setFailed(error.message);
