2c91eec90907b27abf18f07d92a80f51b3c21f83
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-03-26 09:34:11 -0700

    no message

--- src/update-card.ts
+++ src/update-card.ts
@@ -1,4 +1,4 @@
-import core from '@actions/core';
+import * as core from '@actions/core';
 import { addLabels, context, createBranch, createCard, getBranch, getCardByIssue, getColumn, getColumnByName, getIssue, getProjectByName, removeLabel } from './octokit-client';
 import { parallel } from './utils';
 
