ff1c99df0916206991d1aedd03970bf76de7a258
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-04-09 18:09:41 -0700

    no message

--- src/move-card-to-column.ts
+++ src/move-card-to-column.ts
@@ -1,17 +1,26 @@
 
+import { info } from '@actions/core';
 import { context, getCardByIssue, getColumnByName, getProjectByName, moveExistingCard } from './octokit-client';
 
 export default async function moveCardToColumn() {
   const branch = context.payload.pull_request.head.ref;
+  info(`Pull request for branch: ${branch}`);
+
   const issueNumber = branch.split('/').pop();
+  info(`Issue number: ${issueNumber}`);
+
   const project = await getProjectByName('Avia JS Sprint Board');
+  info(`Project: ${JSON.stringify(project, null, 2)}`);
+
   const card = await getCardByIssue(issueNumber, project.number);
+  info(`Card: ${JSON.stringify(card, null, 2)}`);
 
   if (!card) {
     return;
   }
 
   const toColumn = await getColumnByName('Ready for Review', project.number);
+  info(`To Column: ${JSON.stringify(toColumn, null, 2)}`);
 
   moveExistingCard(toColumn.id, card.id);
 }
