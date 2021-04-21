be41a1673fde3df025b2cf6a9203a9118863d6e1
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-04-21 13:39:30 -0700

    Actions (#6)

--- src/move-card-to-column.ts
+++ src/move-card-to-column.ts
@@ -1,13 +1,17 @@
-
 import { getInput, info } from '@actions/core';
 import { context, getCardByIssue, getColumnByName, getProjectByName, moveExistingCard } from './octokit-client';
 
 export default async function moveCardToColumn() {
-  const column = getInput('column');
-  info(`Move to column: ${column}`);
-
   const branch = context.payload.pull_request.head.ref;
   info(`Pull request for branch: ${branch}`);
+
+  if (!/issue\/\d+$/.test(branch)) {
+    info(`Ignoring non-issue PR`);
+    return;
+  }
+
+  const column = getInput('column');
+  info(`Move to column ${column}`);
 
   const issueNumber = branch.split('/').pop();
   info(`Issue number: ${issueNumber}`);
