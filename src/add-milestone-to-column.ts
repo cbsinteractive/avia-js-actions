c129f56311f31aa8594f9001cf79b8d3988fd627
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-05-06 14:53:41 -0700

    no message

--- src/add-milestone-to-column.ts
+++ src/add-milestone-to-column.ts
@@ -1,4 +1,5 @@
 
+import { info } from '@actions/core';
 import { context, getColumnByName, getMilestoneByName, getProjectByName, updateIssue } from './octokit-client';
 import { parallel } from './utils';
 
@@ -7,24 +8,35 @@ export default async function addMilestoneToColumn() {
 
   const milestone = await getMilestoneByName(milestoneName);
   if (!milestone) {
+    info(`Could not find milestone ${milestoneName}`);
     return;
   }
 
   const project = await getProjectByName(projectName);
   if (!project) {
+    info(`Could not find project ${project}`);
     return;
   }
 
   const column = await getColumnByName(columnName, project.number);
   if (!column) {
+    info(`Could not find column ${columnName}`);
     return;
   }
 
   const issues = column.cards.map((card: any) => card.content);
   if (!issues?.length) {
+    info(`Could not find cards from column ${columnName}`);
     return;
   }
 
   const details = { milestone: milestone.number };
-  await parallel(...issues.map((issue: any) => updateIssue(issue.number, details)));
+  await parallel(...issues.map((issue: any) => {
+    try {
+      updateIssue(issue.number, details);
+    }
+    catch (error) {
+      info(`Could not add milestone to issue ${issue.number}`);
+    }
+  }));
 }
