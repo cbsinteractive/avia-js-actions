e54f12a95906920fe56301c4fcbe68877b53d9ff
Dan Sparacio <daniel.sparacio@cbsinteractive.com>
2021-05-19 13:49:56 -0700

    build

--- src/update-card.ts
+++ src/update-card.ts
@@ -1,5 +1,5 @@
 import { info } from '@actions/core';
-import { context, createBranch, createCard, getBranch, getCardByIssue, getColumn, getColumnByName, getIssue, getProjectByName } from './octokit-client';
+import { context, createBranch, getBranch,  getColumn,  getIssue,  } from './octokit-client';
 
 export default async function updateCard() {
   if (!context.payload.changes) {
@@ -15,17 +15,17 @@ export default async function updateCard() {
   const to = await getColumn(card.column_id);
 
   switch (to.name) {
-    case 'Ready for QA':
-      const project = await getProjectByName('Avia JS QA Board');
-      const card = await getCardByIssue(issue_number, project.number);
+    // case 'Ready for QA':
+    //   const project = await getProjectByName('Avia JS QA Board');
+    //   const card = await getCardByIssue(issue_number, project.number);
 
-      if (!card) {
-        const column = await getColumnByName('Ready for Test', project.number);
-        info(`No card exists for the labeled Issue in the project. Attempting to create a card in column ${column.id}, for the Issue with the corresponding id #${issue.id}`);
-        await createCard(column.id, issue.id);
-        info(`Successfully created a new card in column #${column.id}, for the Issue with the corresponding id: ${issue.id}`);
-      }
-      break;
+    //   if (!card) {
+    //     const column = await getColumnByName('Ready for Test', project.number);
+    //     info(`No card exists for the labeled Issue in the project. Attempting to create a card in column ${column.id}, for the Issue with the corresponding id #${issue.id}`);
+    //     await createCard(column.id, issue.id);
+    //     info(`Successfully created a new card in column #${column.id}, for the Issue with the corresponding id: ${issue.id}`);
+    //   }
+    //   break;
 
     case 'In Progress':
       const milestone = issue.milestone.title;
