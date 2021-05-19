6d0016a1a8ebbb3399d690bbcae2b9d929e60da6
Dan Sparacio <daniel.sparacio@cbsinteractive.com>
2021-05-19 14:20:52 -0700

    test

--- src/column-flush.ts
+++ src/column-flush.ts
@@ -1,10 +1,38 @@
-
-import { context} from './octokit-client';
-
+import { context, getColumnByName, getProjectByName } from './octokit-client';
 
 export default async function columnFlush() {
-  //const {fromProject: fromProjectName, fromColumn: fromColumnName, toProject: toProjectName, toColumn: toColumnName } = context.payload.inputs;
+  const {
+    fromProject: fromProjectName,
+    fromColumn: fromColumnName,
+    toProject: toProjectName,
+    toColumn: toColumnName
+  } = context.payload.inputs;
 
-  console.log('xxx', context);
+  const fromProject = await getProjectByName(fromProjectName);
+  const fromColumn = await getColumnByName(fromColumnName, fromProject.number);
+  const toProject = await getProjectByName(toProjectName);
+  const toColumn = await getColumnByName(toColumnName, toProject.number);
+
+  console.log('hello world', fromProject, toProject, fromColumn, toColumn);
+
+  if (!fromColumn || !toColumn) {
+    console.log('no from or no to column');
+    return;
+  }
+
+
+  
+
+
+
+
+
+  // inputs: {
+  //   fromColumn: 'Ready for QA',
+  //   fromProject: 'Avia JS Sprint Board',
+  //   toColumn: 'Ready for Test',
+  //   toProject: 'Avia JS QA Board'
+  // }
+
 
 }
